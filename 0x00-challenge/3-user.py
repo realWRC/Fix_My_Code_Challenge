94023a666cac7e92e94931d310c12d505e203628
Wongani <wonganichulu@gmail.com>
2024-05-28 19:24:07 +0200

    Added fix for 3-user.py

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -13,7 +13,6 @@ class User():
     - password: private string hash in MD5
     """
 
-    __password = None
 
     def __init__(self):
         """
@@ -21,6 +20,7 @@ class User():
         - assigned an unique `id`
         """
         self.id = str(uuid.uuid4())
+        self.__password = None
 
     @property
     def password(self):
@@ -40,7 +40,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -54,7 +54,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
